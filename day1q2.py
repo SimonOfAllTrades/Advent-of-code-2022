4527914de57e77b2cc248d6e78929f952608a0ed
Simon Xu <yhsimon.xu@gmail.com>
2022-12-01 23:41:01 -0500

    day 1 q 2

---
+++ day1q2.py
@@ -0,0 +1,15 @@
+inputFilename = "input/day1q1.txt"
+
+caloriesList = []
+
+with open(inputFilename) as f:
+    calories = 0
+    while(line := f.readline()):
+        if line == '\n':
+            caloriesList.append(calories)
+            calories = 0
+        else:
+            calories += int(line)
+
+caloriesList.sort()
+print(sum(caloriesList[-3:]))
